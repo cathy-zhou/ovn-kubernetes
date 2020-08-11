e9847c40c5c4357d98cc5de7a6b21e8b1b5af4fe
John Richards <jrichards@nvidia.com>
2020-08-11 09:46:44 -0400

    [GIA-1064] Attempt to listen on brbond0
    This change will check for the existence of the brbond0 interface.
    If it exists, then it will be used for listening. If not, we'll
    default to 0.0.0.0.
    
    It also changes the uninstall script to skip out on stopping and
    disabling the ovs-exporter if it isn't enabled.

--- dist/packages/ovs-exporter/deploy/install.sh
+++ dist/packages/ovs-exporter/deploy/install.sh
@@ -4,8 +4,31 @@ set -e
 
 PACKAGE_DIRECTORY=$(dirname $(readlink -f $0))
 INSTALLATION_DIRECTORY=/opt/asgard/ovs-exporter
+LISTENING_INTERFACE=brbond0
+DEFAULT_LISTENING_IP="0.0.0.0"
 
 echo "[INFO] Starting ovs-exporter installation process"
+
+echo "[INFO] Determining IP of interface to listen on"
+LISTENING_IP=""
+if [[ -e /sys/class/net/$LISTENING_INTERFACE ]]; then
+    # Output of ip command will look like this:
+    # 14: brbond0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UNKNOWN group default qlen 1000
+    #     inet 10.0.1.1/16 brd 10.0.255.255 scope global brbond0
+    #     valid_lft forever preferred_lft forever
+    #
+    # We'll extract the IP from that.
+    LISTENING_IP=`ip -4 addr show brbond0 | grep inet | awk '{ print $2; }' | sed 's|/.*||g'`
+    echo "[INFO] Found $LISTENING_INTERFACE"
+else
+    echo "[WARN] Couldn't find $LISTENING_INTERFACE"
+fi
+
+if [[ -z "$LISTENING_IP" ]]; then
+    LISTENING_IP=$DEFAULT_LISTENING_IP
+fi
+
+echo "[INFO] Will listen on $LISTENING_IP"
 
 echo "[INFO] Installing files"
 install -p -m 0755 -D $PACKAGE_DIRECTORY/../bin/ovn-kube-util $INSTALLATION_DIRECTORY/ovn-kube-util
@@ -13,6 +36,7 @@ install -p -m 0644 -D $PACKAGE_DIRECTORY/../bin/git_info $INSTALLATION_DIRECTORY
 
 echo "[INFO] Setting up service"
 install -p -m 0644 $PACKAGE_DIRECTORY/../config/ovs-exporter.service /etc/systemd/system/ovs-exporter.service
+sed -i 's/<LISTENING_IP>/'"$LISTENING_IP"'/' /etc/systemd/system/ovs-exporter.service
 systemctl daemon-reload
 systemctl enable ovs-exporter
 systemctl start ovs-exporter
