7545c5eccd0c8be21c1a0eadd3dde1cd3051e336
Yun Zhou <yunz@nvidia.com>
2022-11-09 11:46:58 -0800

    cni changes

--- go-controller/pkg/node/healthcheck.go
+++ go-controller/pkg/node/healthcheck.go
@@ -318,13 +318,13 @@ func checkForStaleOVSRepresentorInterfaces(nodeName string, wf factory.ObjectCac
 		klog.Errorf("Failed to list pods. %v", err)
 		return
 	}
-	expectedIfaceIds := make(map[string]bool)
+	expectedIfaceIdsWithoutPrefix := make(map[string]bool)
 	for _, pod := range pods {
-		if pod.Spec.NodeName == nodeName {
+		if pod.Spec.NodeName == nodeName && util.PodWantsNetwork(pod) {
 			// Note: wf (WatchFactory) *usually* returns pods assigned to this node, however we dont rely on it
 			// and add this check to filter out pods assigned to other nodes. (e.g when ovnkube master and node
 			// share the same process)
-			expectedIfaceIds[util.GetIfaceId(pod.Namespace, pod.Name)] = true
+			expectedIfaceIdsWithoutPrefix[util.GetIfaceId(pod.Namespace, pod.Name)] = true
 		}
 	}
 
@@ -336,7 +336,19 @@ func checkForStaleOVSRepresentorInterfaces(nodeName string, wf factory.ObjectCac
 				"skipping cleanup check for interface", ifaceInfo.Name)
 			continue
 		}
-		if _, ok := expectedIfaceIds[ifaceId]; !ok {
+		prefix := ""
+		nadName, ok := ifaceInfo.Attributes["network_name"]
+		if ok {
+			prefix = util.GetSecondaryNetworkPrefix(nadName)
+			if !strings.HasPrefix(ifaceId, prefix) {
+				klog.Warningf("iface-id of OVS interface %s for nad %s is invalid: %s", ifaceInfo.Name,
+					nadName, ifaceId)
+				continue
+			}
+			ifaceId = strings.TrimPrefix(ifaceId, prefix)
+		}
+
+		if _, ok := expectedIfaceIdsWithoutPrefix[ifaceId]; !ok {
 			klog.Warningf("Found stale OVS Interface, deleting OVS Port with interface %s", ifaceInfo.Name)
 			_, stderr, err := util.RunOVSVsctl("--if-exists", "--with-iface", "del-port", ifaceInfo.Name)
 			if err != nil {
